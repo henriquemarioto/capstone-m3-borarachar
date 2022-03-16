900576f6bc98cb663cd4cd0e0b033787a156dc07
Brayon França <brayonfr@gmail.com>
2022-03-16 11:42:13 -0300

    style(global.js): create CSS VAR for colours

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,11 +1,44 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-    /* http://meyerweb.com/eric/tools/css/reset/
-    v2.0 | 20110126
-    License: none (public domain)
-    */
-    *{
+
+:root{
+
+    --black: #000000;
+    --black-75: rgba(0, 0, 0, 0.75);
+    --black-50: rgba(0, 0, 0, 0.5);
+    --black-25: rgba(0, 0, 0, 0.25);
+    --white: #FFFFFF;
+    --white-75: rgba(255, 255, 255, 0.75);
+    --white-50: rgba(255, 255, 255, 0.5);
+    --white-15: rgba(255, 255, 255, 0.15);
+    --white-0: rgba(255, 255, 255, 0);
+    --dark: #121212;
+    --light: #F9F9F9;
+    --lighter-gray: #F6F3F3;
+    --light-gray: #DFDFDF;
+    --gray: #C4C4C4;
+    --gray-15: rgba(196, 196, 196, 0.15);
+    --dark-gray: #797979;
+    --darker-blue: #050B2F;
+    --dark-blue: #07387B;
+    --blue-gradient: #07387B - #050B2F ;
+    --grayish-blue: #2E7DB4;
+    --blue: #439AD8;
+    --dark-purple-blue: #171D6D;
+    --purple-blue: #4D47E9;
+    --light-purple-blue: #635EFF;
+    --red: #E74C3C;
+    --light-red: #FD4634;
+    --red-15: rgba(231, 76, 60, 0.15);
+    --green: #07BC0C;
+    --green-15: rgba(7, 188, 12, 0.15);
+    --yellow-15: rgba(241, 196, 15, 0.15);
+    --yellow: #F1C40F;
+    --purple: #7B61FF;
+
+}
+*{
         box-sizing: border-box;
     }
     html, body, div, span, applet, object, iframe,
@@ -29,13 +62,15 @@ export default createGlobalStyle`
         font-family: 'Roboto';
         vertical-align: baseline;
     }
-    /* HTML5 display-role reset for older browsers */
     article, aside, details, figcaption, figure, 
     footer, header, hgroup, menu, nav, section {
         display: block;
     }
     body {
         line-height: 1;
+        background-color: var(--light);
+        min-height: 100vh;
+        min-width: 100vw;
     }
     ol, ul {
         list-style: none;
@@ -52,4 +87,16 @@ export default createGlobalStyle`
         border-collapse: collapse;
         border-spacing: 0;
     }
-`
+    
+
+    
+button, a{
+    cursor: pointer;
+}
+
+
+
+
+
+
+`;
