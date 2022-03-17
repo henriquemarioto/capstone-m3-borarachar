2ea434825f883943ab7f873d117871dc63c126b8
Brayon França <brayonfr@gmail.com>
2022-03-17 10:11:24 -0300

    style(logo): logo component created (images folder created)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import Input from "./components/Input";
 import GlobalStyle from "./styles/global";
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle />
-      <Input inputName={"Name"} isErrored={true} />
     </div>
   );
 }
