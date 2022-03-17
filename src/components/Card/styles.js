f3d684ea000efe85920c88dd5b0841578d7e7dae
Lucas Anacleto <lucasanacleto@outlook.com.br>
2022-03-17 12:33:00 -0300

    feat(cards and popups): create base cards and popups

---
+++ src/components/Card/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  border-radius: 10px;
+  padding: 10px;
+  border: 1px solid #dfdfdf;
+`;
