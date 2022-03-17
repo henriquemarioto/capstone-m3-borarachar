2db1938e9e0624f117aa56fc55141d3047fdb56e
henriquemarioto <henrique.marioto@outlook.com>
2022-03-17 13:46:10 -0300

    fix: on app.jsx router import 2x

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { Routes } from "./routes";
 import GlobalStyle from "./styles/global";
-import Input from "./components/Input";
 import { Routes } from "./routes";
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Routes/>
+      <Routes />
     </div>
   );
 }
