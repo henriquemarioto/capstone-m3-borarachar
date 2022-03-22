4f3e3a9637c8e068e289c14f55ddb82781239eaf
henriquemarioto <henrique.marioto@outlook.com>
2022-03-21 23:29:16 -0300

    feat(searching): in progress

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -2,39 +2,68 @@ import CardGroup from "../../components/Card/CardGroup";
 import CardUser from "../../components/Card/CardUser";
 import Header from "../../components/Header";
 import { Container, ContentGroup, ContentMembers } from "./styles";
-export const Search = () => {
-  return (
-    <>
-      <Header search />
-      <Container>
-        <ContentGroup>
-          <div>
-            <h1>Grupos</h1>
-          </div>
-          {[1,2,3].map((item) => (
-            <CardGroup group={item} />
-          ))}
-          <div>
-            <button className="verMais"> Ver mais ...</button>
-          </div>
-        </ContentGroup>
+import useUser from "../../providers/User";
+import { useState, useEffect } from "react";
+import api from "../../services/api";
+import { useQuery } from "../../Hook";
 
-        <ContentMembers>
-          <div>
-            <h1>Membros</h1>
-          </div>
-          {[profileOwner, profileOwner].map((item) => (
-            <CardUser
-              /*  key={(item.userId = +1)} */
-              perfil={item}
-              type={"userFind"}
-            />
-          ))}
-          <div>
-            <button className="verMais"> Ver mais ...</button>
-          </div>
-        </ContentMembers>
-      </Container>
-    </>
+export const Search = () => {
+  const {
+    user: { token },
+  } = useUser();
+  const [resSearch, setResSearch] = useState({});
+  const query = useQuery();
+
+  const getAndSetData = async () => {
+    setResSearch(
+      (
+        await api.get(`/search?search=${query.get("search")}`, {
+          headers: { authorization: `Bearer ${token}` },
+        })
+      ).data
+    );
+  };
+
+  useEffect(() => {
+    getAndSetData();
+  }, []);
+
+  return (
+    <Container>
+      <ContentGroup>
+        <div>
+          <h1>Grupos</h1>
+        </div>
+        {resSearch.groups?.map((item) => (
+          <CardGroup
+            key={item._id}
+            type="joinGroup"
+            groupData={item}
+            owner={item.owner === item._id}
+            onClick={() => history.push(`/group/${group._id}`)}
+          />
+        ))}
+        <div>
+          <button className="verMais"> Ver mais ...</button>
+        </div>
+      </ContentGroup>
+
+      <ContentMembers>
+        <div>
+          <h1>Membros</h1>
+        </div>
+        {resSearch.users?.map((item) => (
+          <CardUser
+            type="userFind"
+            key={item._id}
+            memberData={item}
+            onClick={() => history.push(`/group/${_id}`)}
+          />
+        ))}
+        <div>
+          <button className="verMais"> Ver mais ...</button>
+        </div>
+      </ContentMembers>
+    </Container>
   );
 };
