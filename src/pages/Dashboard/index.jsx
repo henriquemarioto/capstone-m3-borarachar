ce661465e30d4b7b37176edd30f10c12eb958159
Pedro <pedro@gmail.com>
2022-03-22 18:14:48 -0300

    style(estilizaçao do popup de streamisg termindado): finalizado o style

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -122,7 +122,7 @@ export const Dashboard = () => {
             <Loading />
           )}
         </section>
-        <Popup popUpType={"PopupStreamings"} />
+
         <section>
           {!searchingFoGroupsLoading ? (
             <DashboardSections
